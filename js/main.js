46d5bb91731fdeff83c301d7bffc53bd782cca7f
BRENO@Breno <brenobrito_13@hotmail.com>
2021-12-06 18:33:26 -0300

    bugfix requisição

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 document.getElementById('formulario').addEventListener('submit', pesquisarFilme);
 
 function pesquisarFilme(e){
-    console.log('gelo');
+    var filmePesquisa = document.getElementById('pesquisar').value;
+    buscarFilmes(filmePesquisa);
     e.preventDefault();
+}
+function buscarFilmes(filmePesquisa){
+    axios.get('https://www.omdbapi.com?apikey=623bf177&s=' + filmePesquisa)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
 }
